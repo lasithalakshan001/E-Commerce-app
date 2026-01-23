dee6800c516c39818bd22e3f4e1d0256a28bdd3d
Lasi <lasithalakshan001@gmail.com>
2026-01-23 09:00:16 +0530

    Search bar updated - return

--- src/components/Molecules/SearchBar.jsx
+++ src/components/Molecules/SearchBar.jsx
@@ -1,17 +1,18 @@
-import { Button ,Form, InputGroup } from 'react-bootstrap';
+import { Form, InputGroup, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const SearchBar = () => {
-    <form action="" className="d-flex flex-grow mx-3">
-        <InputGroup>
-            <Form.Control type='search' placeholder="search product"/>
-
-            <Button varient="outline-primary">
-                <i className='bi bi-search'></i>
-            </Button>
-        </InputGroup>
-    </form>
+    return (
+        <Form className="d-flex flex-grow-1 mx-3">
+            <InputGroup>
+                <Form.Control type='search' placeholder='Search Products'/>
+                <Button variant='outline-primary'>
+                    <i className='bi bi-search'></i>
+                </Button>
+            </InputGroup>
+        </Form>
+    );
 }
 
 export default SearchBar;
